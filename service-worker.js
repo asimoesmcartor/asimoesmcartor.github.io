2f3841317d98cfcbb61263a7033ef817827ed02f
Alex Simoes-McArtor <asimoesmcartor@gmail.com>
2019-06-15 21:33:50 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.178aba7ab85e2fe2c1da27660165f0e4.js"
+  "/precache-manifest.55c23013a4f824f3c2f5efb83559459c.js"
 );
 
 workbox.clientsClaim();
